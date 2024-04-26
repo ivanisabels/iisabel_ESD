cad7ea20c657306fa9b664b402d53761b3547802
FabianCastroP <137215240+FabianCastroP@users.noreply.github.com>
2024-04-26 17:54:44 +0200

    Update main.cpp

--- Servidor/main.cpp
+++ Servidor/main.cpp
@@ -1,52 +1,48 @@
 #include "client.h"
+#include "iot_server.h"
+#include <iostream>
+#include <cstring>
 
-// Client side implementation of UDP client-server model 
-#include <bits/stdc++.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
-   
-#define PORT     8080 
-#define MAXLINE 1024 
-   
-// Driver code 
-int main() { 
-    int sockfd; 
-    char buffer[MAXLINE]; 
-    const char *hello = "Hello from client"; 
-    struct sockaddr_in     servaddr; 
-   
-    // Creating socket file descriptor 
-    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-        perror("socket creation failed"); 
-        exit(EXIT_FAILURE); 
-    } 
-   
-    memset(&servaddr, 0, sizeof(servaddr)); 
-       
-    // Filling server information 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_port = htons(PORT); 
-    servaddr.sin_addr.s_addr = INADDR_ANY; 
-       
-    int n;
-    socklen_t len; 
-       
-    sendto(sockfd, (const char *)hello, strlen(hello), 
-        MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
-            sizeof(servaddr)); 
-    std::cout<<"Hello message sent."<<std::endl; 
-           
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
-                MSG_WAITALL, (struct sockaddr *) &servaddr, 
-                &len); 
-    buffer[n] = '\0'; 
-    std::cout<<"Server :"<<buffer<<std::endl; 
-   
-    close(sockfd); 
-    return 0; 
+int main() {
+    // Server details
+    std::string server_address = "127.0.0.1";
+    int server_port = 8080;
+
+    // Client details
+    std::string client_address = "127.0.0.1";
+    int client_port = 8888;
+
+    // Create UDP server
+    udp_client_server::udp_server server(server_address, server_port);
+    std::cout << "UDP server is running on " << server.get_addr() << ":" << server.get_port() << std::endl;
+
+    // Create UDP client
+    udp_client_server::udp_client client(client_address, client_port);
+    std::cout << "UDP client is running on " << client.get_addr() << ":" << client.get_port() << std::endl;
+
+    // Message to send
+    const char* message = "Hello from client!";
+    size_t message_size = std::strlen(message);
+
+    // Send message from client to server
+    int bytes_sent = client.send(message, message_size);
+    if (bytes_sent < 0) {
+        std::cerr << "Error sending message from client to server" << std::endl;
+        return 1;
+    }
+
+    // Receive message from server
+    char buffer[1024];
+    int bytes_received = server.recv(buffer, sizeof(buffer));
+    if (bytes_received < 0) {
+        std::cerr << "Error receiving message from server" << std::endl;
+        return 1;
+    }
+
+    // Print received message
+    buffer[bytes_received] = '\0';
+    std::cout << "Received message from server: " << buffer << std::endl;
+
+    return 0;
 }
+
