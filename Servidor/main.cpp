3b2f71ea0ca7d48cd96d47e6b81354cc6b5c7477
ivanisabels <ivan.isabel.sierra@alumnos.upm.es>
2024-05-01 23:39:47 +0200

    i2c y main

--- Servidor/main.cpp
+++ Servidor/main.cpp
@@ -1,23 +1,19 @@
 #include "client.h"
-#include "iot_server.h"
 #include <iostream>
 #include <cstring>
 
 int main() {
     // Server details
-    std::string server_address = "127.0.0.1";
-    int server_port = 8080;
+    std::string server_address = "192.168.1.168";
+    int server_port = 5000;
 
     // Client details
-    std::string client_address = "127.0.0.1";
+    std::string client_address = "192.168.1.128";
     int client_port = 8888;
 
-    // Create UDP server
-    udp_client_server::udp_server server(server_address, server_port);
-    std::cout << "UDP server is running on " << server.get_addr() << ":" << server.get_port() << std::endl;
 
     // Create UDP client
-    udp_client_server::udp_client client(client_address, client_port);
+    udp_client client(server_address, server_port);
     std::cout << "UDP client is running on " << client.get_addr() << ":" << client.get_port() << std::endl;
 
     // Message to send
@@ -30,18 +26,6 @@ int main() {
         std::cerr << "Error sending message from client to server" << std::endl;
         return 1;
     }
-
-    // Receive message from server
-    char buffer[1024];
-    int bytes_received = server.recv(buffer, sizeof(buffer));
-    if (bytes_received < 0) {
-        std::cerr << "Error receiving message from server" << std::endl;
-        return 1;
-    }
-
-    // Print received message
-    buffer[bytes_received] = '\0';
-    std::cout << "Received message from server: " << buffer << std::endl;
 
     return 0;
 }
