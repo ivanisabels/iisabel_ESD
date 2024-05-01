a7a1562cb062ea6015c9b82d161331280139fbb3
ivanisabels <ivan.isabel.sierra@alumnos.upm.es>
2024-05-01 23:52:37 +0200

    i2c y main

--- Servidor/client.cpp
+++ Servidor/client.cpp
@@ -27,12 +27,32 @@ udp_client::udp_client(const std::string& addr, int port)
     : f_port(port)
     , f_addr(addr)
 {
+    char decimal_port[16];
+    snprintf(decimal_port, sizeof(decimal_port), "%d", f_port);
+    decimal_port[sizeof(decimal_port) / sizeof(decimal_port[0]) - 1] = '\0';
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_protocol = IPPROTO_UDP;
+    
+    int r(getaddrinfo(addr.c_str(), decimal_port, &hints, &f_addrinfo));
+    if(r != 0 || f_addrinfo == NULL)
+    {
+        throw udp_client_server_runtime_error(("invalid address or port: \"" + addr + ":" + decimal_port + "\"").c_str());
+    }
+    f_socket = socket(f_addrinfo->ai_family, SOCK_DGRAM | SOCK_CLOEXEC, IPPROTO_UDP);
+    if(f_socket == -1)
+    {
+        freeaddrinfo(f_addrinfo);
+        throw udp_client_server_runtime_error(("could not create socket for: \"" + addr + ":" + decimal_port + "\"").c_str());
+    }
 
     //Creation socket file descriptor
-    if( f_socket = socket(AF_INET, SOCK_DGRAM, 0) < 0){
-        udp_client_server_runtime_error("Creation socket failed");
-        close(f_socket);
-    }
+    //if( f_socket = socket(AF_INET, SOCK_DGRAM, 0) < 0){
+      //  udp_client_server_runtime_error("Creation socket failed");
+        //close(f_socket);
+    //}
 
     //Initialice the accelerometer
     //I2CDevice::init_i2c(1,MPU_ADDR);
